466a133b332b5ecd6300988bf5a9a1cdf2169f45
Mizut452 <ssvror444@gmail.com>
2023-01-23 01:41:28 +0900

    ログインコントローラーの作成

--- src/main/java/Mizut452/learn_law/Controller/LoginController.java
+++ src/main/java/Mizut452/learn_law/Controller/LoginController.java
@@ -1,4 +1,29 @@
 package Mizut452.learn_law.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController {
+
+    @RequestMapping("/login")
+    public String Login() {
+        return "Login/login";
+    }
+
+    @RequestMapping("/createaccount")
+    public String createAccount() {
+        return "Login/createAccount";
+    }
+
+    @PostMapping("/createaccount/create")
+    public String createMethod() {
+        return "redirect:/createaccount/roles";
+    }
+
+    @PostMapping("/createaccount/roles")
+    public String createRoles() {
+        return "Login/Complete";
+    }
 }
