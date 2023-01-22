3fe1005c527a3572bed955eaacfeb1ac0b89c86d
Mizut452 <ssvror444@gmail.com>
2023-01-23 01:45:19 +0900

    SecurityConfigの作成

---
+++ src/main/java/Mizut452/learn_law/LoginSecurity/SecurityConfig.java
@@ -0,0 +1,41 @@
+package Mizut452.learn_law.LoginSecurity;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .defaultSuccessUrl("/quiz")
+                .failureUrl("/login?error")
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutSuccessUrl("/quiz")
+        ).authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/").permitAll()
+                .anyRequest().permitAll()
+        );
+        return http.build();
+    }
+
+    //パスワードのハッシュ化
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
